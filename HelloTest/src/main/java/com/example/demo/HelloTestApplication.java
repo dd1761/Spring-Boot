7e32a0a7c16b7477121a9da43fa5a103fc1d9afb
KIMDONGBEEN <dd1761@naver.com>
2023-05-02 10:50:20 +0900

    23-05-02
    Gradle로 프로젝트 생성해보기

--- HelloTest/src/main/java/com/example/demo/HelloTestApplication.java
+++ HelloTest/src/main/java/com/example/demo/HelloTestApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	//스프링 부트로 만든 애플리케이션의 시작 클래스임을 의미
 public class HelloTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloTestApplication.class, args);
+		
+		System.out.println("Hello SpringBoot!!");
 	}
 
 }
