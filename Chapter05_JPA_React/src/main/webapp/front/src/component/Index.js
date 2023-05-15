5d866c2b43dfb3224b6f1bdafdc74c8f3369ce5b
KIMDONGBEEN <dd1761@naver.com>
2023-05-15 17:54:33 +0900

    23-05-15
    Spring Boot에서 vscode로 React연결 완료~

---
+++ Chapter05_JPA_React/src/main/webapp/front/src/component/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h3>***메인 화면***</h3>
+            <hr />
+        </div>
+    );
+};
+
+export default Index;
