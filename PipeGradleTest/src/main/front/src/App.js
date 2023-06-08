872ee34e1c50ba2f88a1663fa44e8a78fc7dd27d
KIMDONGBEEN <dd1761@naver.com>
2023-06-08 09:51:28 +0900

    build.gradle 오류 수정

--- PipeGradleTest/src/main/front/src/App.js
+++ PipeGradleTest/src/main/front/src/App.js
@@ -1,61 +1,55 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
+import img1 from './1.jpg';
+import './App.css';
 
-import img1 from './mokoko.webp';
+function App() {
+    const [message, setMessage] = useState([]);
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        console.log("useEffect");
+        fetch("/hello")
+            .then((response) => {
+                console.log("hello");
+                return response.json();
+            })
+            .then(function (data) {
+                setMessage(data);
+            });
 
-const App = () => {
+        axios.get("/list")
+             .then(res=>{
+                console.log("list");
+                setData(res.data);
+                console.log(res.data.length);
+              })
+    }, []);
 
-  const [message,setMessage] = useState([]);
-  const [data,setData] = useState([]);
-
-  useEffect(() => {
-    fetch('/hello')
-    .then(res => { return res.json()})
-    .then(function(data){
-      setMessage(data);
-    });
-
-    axios.get('/list').then(
-      res => {
-        setData(res.data);
-      }
-    )  
-  }, []);
-  
-  return (
-    <div>
-      <header>
-        <img src={img1} style = {{width:'200px' }}/>
-        <h1>스프링 부트로 /hello부터 받은 데이터 출력</h1>
-        <ul style = {{listStyle: 'none'}}>
-          {
-         message.map((item,index) => <li key = {index}>
-            {item}
-
-          
-          </li>)
-          }
-
-
-        </ul>
-        <hr/>
-
-        <section>
-            <h2>스프링 부트로 /list부터 받은 데이터 출력</h2>
-            {
-              data.map((item,index) => <li key = {index}>
-                {item.name} : {item.addr} : {item.age}
-
-
-              </li>)
-            }
-
-
-        </section>
-
-      </header>
-    </div>
-  );
-};
+    return (
+        <div className="App">
+            <header>  
+                <img src={ img1 } style={{ width: '200px' }}/>   
+                <h1>Test-1</h1>          
+                <ul style={{ listStyle: 'none' }}>
+                  {
+                    message.map((text, index) => <li key={ index }>
+                        { text }
+                      </li>)
+                  }
+                </ul>
+            </header>
+            <hr/>
+            <section>
+                <h2>SpringBoot /list 로부터 받은 데이타 출력</h2>
+                {
+                  data.map((obj, index) => <li key={ index }>
+                    { obj.name } : { obj.addr } 거주({ obj.age }세)
+                  </li>)
+                }
+            </section>
+        </div>
+    );
+}
 
 export default App;
